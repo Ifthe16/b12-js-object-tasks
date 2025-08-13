d2b9e264a2ad1aa607ff576d40b5c3cc78bd6efa
Ifthe16 <habiburrahmanif975@gmail.com>
2025-08-13 18:05:38 +0600

    added task-04.js and task-05.js files

---
+++ task-04.js
@@ -0,0 +1,10 @@
+/* Task-04 */
+let student = {
+    name: 'Ariana Grande',
+    age: 21,
+    city: 'Gaibandha',
+    isStudent: true
+};
+
+const keysCount = Object.keys(student).length;
+console.log(keysCount);
