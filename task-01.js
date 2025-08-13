666bff4ad381a2080b48f5253e39c56f6487222a
Ifthe16 <habiburrahmanif975@gmail.com>
2025-08-13 17:42:42 +0600

    added task-01.js and task-02.js files

---
+++ task-01.js
@@ -0,0 +1,10 @@
+/* Task-01 */
+const colors = {
+    red: "#ff0000",
+    green: "#00ff00",
+    blue: "#0000ff",
+    "golden rod": '#daa520'
+};
+
+const propName = "golden rod";
+console.log(colors[propName]);
