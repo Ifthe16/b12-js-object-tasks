95a424b2a0f745c1ef9dabdd4152a6021c37d02c
Ifthe16 <habiburrahmanif975@gmail.com>
2025-08-13 17:47:28 +0600

    added task-03.js file

---
+++ task-03.js
@@ -0,0 +1,13 @@
+/* Task-03 */
+const student = {
+    name: "Hamim Sakep",
+    id: 5421,
+    physics: {
+        subject: "HSC Physics",
+        author: "Shahjahan Tapan",
+        marks: 30
+    }
+};
+
+const keyName = student.physics['marks'];
+console.log(keyName);
